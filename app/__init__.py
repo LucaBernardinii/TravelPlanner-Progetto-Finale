e8c5ebeb2330f3c6c42232321a65b3b1dc49d7b7
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2026-05-18 17:47:51 +0200

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from config import Config
+import os
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    app = Flask(__name__, instance_relative_config=True)
+
+    # MODIFICA QUI:
+    # os.environ.get('NOME_VAR', 'valore_default')
+    # Se trova SECRET_KEY nel sistema/file .env la usa.
+    # Altrimenti usa 'dev' (utile per non bloccarci se manca il file).
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
+        DATABASE=os.path.join(app.instance_path, 'blog.sqlite'),
+    )
 
     from app.blueprints.auth import auth_bp
     from app.blueprints.trips import trips_bp
